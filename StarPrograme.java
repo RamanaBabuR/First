8d8cc331eb1edd4b26397ccd8e9ca85e0e285270
RamanaBabuR <113959329+RamanaBabuR@users.noreply.github.com>
2022-09-20 08:53:15 +0530

    Update StarPrograme.java

--- StarPrograme.java
+++ StarPrograme.java
@@ -14,7 +14,7 @@ public class StarPrograme {
 	}//new 
 	public void reverse() {
 		
-		String str= "Gokila";
+		String str= "Reverse";
 		
 		for(int i=str.length()-1;i>=0;i--) {
 			System.out.print(str.charAt(i));
