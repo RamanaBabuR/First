3354f3dc32535d7d64e2e6e18d1976a489f4a5e9
RamanaBabuR <113959329+RamanaBabuR@users.noreply.github.com>
2022-09-20 08:47:09 +0530

    Add files via upload

---
+++ StarPrograme.java
@@ -0,0 +1,30 @@
+package practiceJava;
+
+public class StarPrograme {
+	
+	public void downStar() {
+		
+		for(int row= 1; row<=5; row++) {
+			for(int col=1; col<=row; col++) {
+					System.out.print("* ");
+			}
+			System.out.println("");
+		}
+		
+	}
+	public void reverse() {
+		
+		String str= "Gokila";
+		
+		for(int i=str.length()-1;i>=0;i--) {
+			System.out.print(str.charAt(i));
+		}			
+		
+	}
+	public static void main(String[] args) {
+		StarPrograme S = new StarPrograme();
+		S.downStar();
+		S.reverse();
+	}
+
+}
